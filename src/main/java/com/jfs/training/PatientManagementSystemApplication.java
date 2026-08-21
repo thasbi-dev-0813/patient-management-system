390fa624adcfe5ddd20e4ad259303fe958d9eefd
thasbi <thasbi0813@gmail.com>
2026-08-21 19:45:34 +0530

    Test jenkins polling

--- src/main/java/com/jfs/training/PatientManagementSystemApplication.java
+++ src/main/java/com/jfs/training/PatientManagementSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PatientManagementSystemApplication {
 
+	// Patient Management System
 	public static void main(String[] args) {
 		SpringApplication.run(PatientManagementSystemApplication.class, args);
 	}
