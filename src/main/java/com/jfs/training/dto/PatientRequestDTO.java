8c836fc14a1093eb6cef07a2c78b81a5f2ada536
thasbi <thasbi0813@gmail.com>
2026-08-24 16:45:36 +0530

    Add Lombok to remove duplication

--- src/main/java/com/jfs/training/dto/PatientRequestDTO.java
+++ src/main/java/com/jfs/training/dto/PatientRequestDTO.java
@@ -1,10 +1,17 @@
 package com.jfs.training.dto;
-import java.time.LocalDate;
 
+import java.time.LocalDate;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PatientRequestDTO {
 
     @NotBlank(message = "Name is required")
@@ -16,62 +23,12 @@ public class PatientRequestDTO {
 
     @NotBlank(message = "Phone number is required")
     @Pattern(
-        regexp = "^\\d{10}$",
+        regexp = "^[0-9]{10}$",
         message = "Phone number must contain 10 digits"
     )
     private String phone;
 
     private LocalDate dateOfBirth;
-
     private String gender;
-
     private String address;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
